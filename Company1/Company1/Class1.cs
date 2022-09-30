f37c82f4024130e44edc88d914afc386ba0214f7
jyothivmatam <113531577+jyothivmatam@users.noreply.github.com>
2022-09-30 06:47:04 +0530

    Add files via upload

---
+++ Company1/Company1/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company1
+{
+
+    class Company
+    {
+        private int ID;
+        private string Name;
+        private string Address;
+        private int Scale;
+
+
+        public Company()
+        {
+            Console.WriteLine("good company");
+        }
+        public int Id { get { return ID; } set { ID = value; } }
+        
+            
+        
+        public string name { get { return Name; } set { Name = value; } }
+        
+           
+        
+        public string address { get { return Address; } set { Address = value; } }
+        
+  
+        public int scale { get { return Scale; } set { Scale = value; } }
+        
+          
+        
+        public void Gettype()
+        {
+            Console.WriteLine("Service Based Company");
+
+        }
+        public void GetTechnologies()
+        {
+            Console.WriteLine("C Sharp");
+            Console.WriteLine("Dot Net");
+            Console.WriteLine("Azure");
+        }
+    }
+}
+
+
+
+
---
+++ Company1/Company1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company1
+{
+  class program
+    {
+
+        static void Main(string[] args)
+        {
+            Company company = new Company();
+
+            company.Id = 123;
+            company.name="Moback";
+            company.address="Bangalore";
+            company.scale=100;
+            Console.WriteLine(company.Id);
+            Console.WriteLine(company.name);
+            Console.WriteLine(company.address);
+            Console.WriteLine(company.scale);
+            company.Gettype();
+            company.GetTechnologies();
+            Console.ReadLine();
+
+        }
+    }
+}
