b081ee0f5c6374bd434405028b1a5cc3da9a3d75
jyothivmatam <113531577+jyothivmatam@users.noreply.github.com>
2022-10-03 22:47:16 +0530

    Add files via upload

---
+++ absract/absract/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace absract
+{
+   public abstract class Math
+    {
+        public void Add(int x,int y)
+        {
+            Console.WriteLine($"Addition of {x} and {y} is {x + y}");
+
+        }
+        public void Sub(int x, int y)
+            {
+                Console.WriteLine($"Subtraction of {x} and {y} is : {x - y}");
+            }
+        public abstract void Mul(int x, int y);
+          public abstract void Div(int x, int y);
+        }
+        public class MathPro : Math
+        {
+            public override void Mul(int x, int y)
+            {
+                Console.WriteLine($"Multiplication of {x} and {y} is : {x * y}");
+            }
+            public override void Div(int x, int y)
+            {
+                Console.WriteLine($"Division of {x} and {y} is : {x / y}");
+            }
+            public void Mod(int x, int y)
+            {
+                Console.WriteLine($"Modulos of {x} and {y} is : {x % y}");
+            }
+        }
+
+    }
+
---
+++ absract/absract/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace absract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathPro mathpro=new MathPro();
+            Math math =mathpro;
+            math.Add(5, 10);
+            math.Sub(60, 50);
+            math.Div(5,10);
+            math.Mul(60,10);
+
+        }
+    }
+}
