f8cbfd3b1607d850d0c1006d67f26559aca00bb7
jyothivmatam <113531577+jyothivmatam@users.noreply.github.com>
2022-10-03 22:49:59 +0530

    Add files via upload

---
+++ Inheritance/Inheritance/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Inheritance
+{
+    class RegularPolygon
+    {
+        public  void CalucalatePerimeter(int length, int sides)
+        {
+            int result = length * sides;
+            Console.WriteLine("The perimeter is " + result);
+
+        }
+    }
+
+
+    class Square : RegularPolygon
+    {
+        public int length = 300;
+        public int sides = 4;
+        public void CalucalateArea()
+        {
+            int area = length * length;
+            Console.WriteLine("The area of square is " + area);
+        }
+    }
+    class Rectangle : RegularPolygon
+    {
+        public int length = 200;
+        public int breadth = 300;
+        public int sides = 4;
+        public void CalucalateArea()
+        {
+            int area = length * breadth;
+            Console.WriteLine("The area of Rectangle is " + area);
+
+        }
+    }
+    class Triangle : RegularPolygon
+    {
+        public int length = 80;
+        public int height = 60;
+        public int sides = 3;
+        public void CalucalateArea()
+        {
+            double area = 0.5 * length * height;
+            Console.WriteLine("The area of triangle is " + area);
+        }
+
+    }
+}
+    
+    
+
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
+
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Square s1=new Square();
+            s1.CalucalateArea();
+            s1.CalucalatePerimeter( s1.length, s1.sides);
+
+            Rectangle r1=new Rectangle();
+            r1.CalucalateArea();
+            r1.CalucalatePerimeter( r1.length, r1.sides);
+
+            Triangle triangle=new Triangle();
+            triangle.CalucalateArea();
+            triangle.CalucalatePerimeter( triangle.length, triangle.sides);
+
+        }
+    }
+}
