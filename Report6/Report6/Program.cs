00c95bb16dbb134aac5ba31e62f06fe4260957b4
jyothivmatam <113531577+jyothivmatam@users.noreply.github.com>
2022-10-06 22:25:50 +0530

    Add files via upload

---
+++ Report6/Report6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Report6.Rectangle;
+using static Report6.Rectangle.Circle;
+using static Report6.Rectangle.Circle.Triangle;
+using static Report6.Rectangle.Circle.Triangle.Cone;
+
+namespace Report6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(10, 20);
+            Console.WriteLine("The area of Rectangle is " + rect.GetArea());
+
+            Circle cir = new Circle(20);
+            Console.WriteLine("The area of Circle is " + cir.GetArea());
+           
+                
+            Triangle tria = new Triangle(10, 30);
+            Console.WriteLine("The area of triangle is " +tria.GetArea());
+
+            Cone co = new Cone(20, 10);
+            Console.WriteLine("The area of Cone is " +co.GetArea());
+
+            Square sq = new Square(10);
+            Console.WriteLine("The area of Square is " +sq.GetArea());  
+
+
+
+        }
+    }
+}
---
+++ Report6/Report6/Shape.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Report6
+{
+   public  abstract class Shape
+    {
+        public double Height;
+        public double Width;
+        public double Radius;
+        public const float PI = 3.14f;
+        public abstract double GetArea();
+
+    }
+    public class Rectangle : Shape
+    {
+        public Rectangle(double Height,double Width)
+        {
+            this.Height = Height;
+            this.Width = Width;
+        }
+        public override double GetArea()
+        {
+            return Height * Width;
+        }
+    public class Circle : Shape
+        {
+            public Circle(double Radius)
+            {
+                this.Radius = Radius;
+
+            }
+            public override double GetArea()
+            {
+                return PI * Radius * Radius;
+            }
+    public class Triangle : Shape
+            {
+               public Triangle(double Height,double Width)
+                {
+                    this.Width=Width;
+                    this.Height=Height;
+                }
+                public override double GetArea()
+                {
+                    return Height * Width / 2;
+                }
+    public class Cone : Shape
+                {
+                    public Cone(double Radius , double Height)
+                    {
+                        this.Radius=Radius;
+                        this.Height=Height;
+                    }
+                    public override double GetArea()
+                    {
+                        return PI * Radius*(Math.Sqrt((Radius * Radius) + (Height * Height)));
+                    }
+    public class Square : Shape
+                    {
+                        public Square(double Width)
+                        {
+                            this.Width = Width;
+                        }
+                        public override double GetArea()
+                        {
+                            return Width * Width;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
