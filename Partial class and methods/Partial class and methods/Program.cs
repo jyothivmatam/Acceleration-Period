3436871413fb8df933a3e593c06e5d6ef73ffa89
jyothivmatam <113531577+jyothivmatam@users.noreply.github.com>
2022-10-06 22:27:08 +0530

    Add files via upload

---
+++ Partial class and methods/Partial class and methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_class_and_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
