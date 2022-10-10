fa5dd1a05eed583d1ea8ea28389f7ea253d52de4
jyothivmatam <113531577+jyothivmatam@users.noreply.github.com>
2022-10-10 21:56:25 +0530

    Add files via upload

---
+++ EH1/EH1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EH1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1, Number2, Result;
+            try
+            {
+                Console.WriteLine("Enter First Number:");
+                Number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Second Number:");
+                Number2 = int.Parse(Console.ReadLine());
+                Result = Number1 / Number2;
+                Console.WriteLine($"Result = {Result}");
+            }
+            catch 
+            {
+                Console.WriteLine("Some Error Occurred...");
+            }
+            Console.ReadKey();
+        }
+    }
+}
